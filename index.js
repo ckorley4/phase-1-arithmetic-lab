de8ce91a3c3a32c4d90881b8eb06e077805b8ad7
ckorley4 <ckorley4uo@yahoo.com>
2023-12-28 03:27:32 -0500

    Arithmetic Lab

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-
 // Write your code here
+const num1 = 31
+const num2 = 2
+const multiply = num1 * num2
+console.log(multiply)
+
+const random = Math.ceil(Math.random() * 10)
+console.log(random)
+
+const num3 = 11
+const num4 = 7
+const mod = num3 % num4
+
+const max = Math.max(13, 4, 2, 6, 9, 20, 1, 3)
